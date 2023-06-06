bfe0e17f40150a8b8327fab6ea7f43b898b57f2a
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-06 16:27:51 +0000

    index amd mgmt update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,7 +2,7 @@
 // const selectElement = (selector) => {
 //     const element = document.querySelector(selector);
 //     if(element) return element;
-//     throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+//     throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);
 // };
 
 // //Nav styles on scroll
@@ -61,32 +61,32 @@
 //     }
 // });
 
-// // Swiper
-// const swiper = new Swiper(".swiper", {
-//     // How many slides to show
-//     slidesPerView: 1,
-//     // How much space between slides
-//     spaceBetween: 20,
-//     // Make the next and previous buttons work
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//     // Make the pagination indicators work
-//     pagination: {
-//         el: '.swiper-pagination'
-//     },
-//     //Responsive breakpoints for how many slides to show at that view
-//     breakpoints: {
-//         // 700px and up shoes 2 slides
-//         700: {
-//           slidesPerView: 1
-//         },
-//         // 1200px and up shoes 3 slides
-//         1200: {
-//             slidesPerView: 1
-//         }
-//     }   
-// });
+// Swiper
+const swiper = new Swiper(".swiper", {
+  // How many slides to show
+  slidesPerView: 1,
+  // How much space between slides
+  spaceBetween: 20,
+  // Make the next and previous buttons work
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Make the pagination indicators work
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  //Responsive breakpoints for how many slides to show at that view
+  breakpoints: {
+    // 700px and up shoes 2 slides
+    700: {
+      slidesPerView: 1,
+    },
+    // 1200px and up shoes 3 slides
+    1200: {
+      slidesPerView: 1,
+    },
+  },
+});
 
 // Get all the buttons with the class "viewBtn"
