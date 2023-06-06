bd00e742fcd146ce0d9b6e9f5da9888b78a88c6a
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-06 19:07:04 +0000

    artiste update

--- public/assets/js/mgmt.js
+++ public/assets/js/mgmt.js
@@ -1,19 +1,4 @@
-//using selectors inside the element
-const questions = document.querySelectorAll(".question");
-
-questions.forEach(function (question) {
-  const btn = question.querySelector(".question-btn");
-  // console.log(btn);
-
-  btn.addEventListener("click", function () {
-    // console.log(question);
-
-    questions.forEach(function (item) {
-      if (item !== question) {
-        item.classList.remove("show-text");
-      }
-    });
-
-    question.classList.toggle("show-text");
-  });
-});
+function openArtistPage() {
+  var artistUrl = "AJepk.html"; // Replace with the actual file name and path of the artist page HTML file
+  window.open(artistUrl, "_blank");
+}
